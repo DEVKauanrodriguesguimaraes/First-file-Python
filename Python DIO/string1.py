32ecf07fe6ac30a8f106e20105467624ac56367e
Kauan <kauan18rodrigues@outlook.com>
2024-03-07 23:02:50 -0300

    Conhecendo funções úteis

---
+++ Python DIO/string1.py
@@ -0,0 +1,20 @@
+## Conhecendo métodos utéis na classe string
+nome = "kaUan"
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+## Eliminando espaço em branco
+texto = "  Olá Mundo    "
+print(texto + ".")
+print(texto.strip()+ ".")
+print(texto.rstrip()+ ".")
+print(texto.lstrip()+ ".")
+
+## Junções e centralização
+menu = "Python"
+print("##"+ menu + "##")
+print(menu.center(10, "#"))
+print(menu.center(10))
+print("P-y-t-h-o-n")
+print("-".join(menu))
